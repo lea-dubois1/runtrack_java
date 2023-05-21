c37fd9c3549c608f2ee348d1c6bd061fad818eab
lea-dubois1 <lea.dubois@laplateforme.io>
2023-05-21 16:45:55 +0200

    Job 1, 2, 3 jour 1 terminés

---
+++ jour01/job03/HelloWorld.java
@@ -0,0 +1,11 @@
+package job03;
+
+public class HelloWorld {
+    // Ceci est un commentaire sur une ligne
+    /* Ceci est un commentaire sur
+        plusieurs lignes
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World !");
+    }
+}
---
+++ jour01/job04/DeclarationVariable.java
@@ -0,0 +1,7 @@
+package job04;
+
+public class DeclarationVariable {
+    public static void main(String[] args) {
+        System.out.print("Hello World !");
+    }
+}

9f4187471a24f3b970c149d23b3e351b4774ac94
lea-dubois1 <lea.dubois@laplateforme.io>
2023-09-07 14:07:09 +0200

    job 3 terminé

---
+++ jour2/job03/src/AnimalExercice.java
@@ -0,0 +1,11 @@
+public class AnimalExercice {
+    public static void main(String[] args) {
+        Chat cat = new Chat();
+        Chien dog = new Chien();
+        Oiseau bird = new Oiseau();
+
+        cat.faireDuBruit();
+        dog.faireDuBruit();
+        bird.faireDuBruit();
+    }
+}
---
+++ jour2/job03/src/Oiseau.java
@@ -0,0 +1,6 @@
+public class Oiseau extends Animal{
+    @Override
+    public void faireDuBruit() {
+        System.out.println("L'oiseau chante.");
+    }
+}

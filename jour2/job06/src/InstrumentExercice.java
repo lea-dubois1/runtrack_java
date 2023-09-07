8eeae772d3de64d48fc75fe5a0fc35d713768ba3
lea-dubois1 <lea.dubois@laplateforme.io>
2023-09-07 15:37:58 +0200

    job 6 terminé

---
+++ jour2/job06/src/InstrumentExercice.java
@@ -0,0 +1,9 @@
+public class InstrumentExercice {
+    public static void main(String[] args) {
+        Guitare guitare = new Guitare();
+        Piano piano = new Piano();
+
+        guitare.jouer();
+        piano.jouer();
+    }
+}
---
+++ jour2/job06/src/Piano.java
@@ -0,0 +1,6 @@
+public class Piano implements Instrument {
+    @Override
+    public void jouer() {
+        System.out.println("Le piano joue une mélodie douce.");
+    }
+}

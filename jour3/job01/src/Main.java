4a8ea1083a56757f02b227b3bcce70e4a2b37adb
lea-dubois1 <lea.dubois@laplateforme.io>
2023-09-08 10:23:32 +0200

    job 1 terminé

---
+++ jour3/job01/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        int total = 0;
+
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+
+        for (String inpNum : args) {
+            int num = Integer.parseInt(inpNum);
+            numList.add(num);
+            if(num % 2 == 0) total += num;
+        }
+
+        System.out.println("Somme des nombres pairs: " + total);
+    }
+}

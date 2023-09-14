6bcd75491516d0ab9faffcee494fabb543a89b9e
lea-dubois1 <lea.dubois@laplateforme.io>
2023-09-14 18:20:58 +0200

    job 3 modifié

--- jour4/job03/src/Banque.java
+++ jour4/job03/src/Banque.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Banque {
+public class Banque {
+    private int solde;
+
+    public Banque(int inputSolde) {
+        this.solde = inputSolde;
+    }
+
+    public synchronized void retire(int amount) {
+        this.solde -= amount;
+        System.out.println(Thread.currentThread().getName() + " a retiré " + amount);
+    }
+
+    public void display() {
+        System.out.println("Solde final: " + this.solde);
+    }
 }
--- jour4/job03/src/SynchronizationExercice.java
+++ jour4/job03/src/SynchronizationExercice.java
@@ -1,5 +1,20 @@
-public class Main {
+public class SynchronizationExercice {
     public static void main(String[] args) {
+        Banque banque = new Banque(200);
 
+        Thread thread1 = new Thread(() -> banque.retire(50));
+        Thread thread2 = new Thread(() -> banque.retire(50));
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        banque.display();
     }
 }
